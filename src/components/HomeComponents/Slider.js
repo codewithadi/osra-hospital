b7d20046a0bb2e7f397b9a4df19fa169b0845ae8
ajfuturistics <ajfuturistics@gmail.com>
2021-12-09 19:57:41 +0530

    added slider in Homepage

---
+++ src/components/HomeComponents/Slider.js
@@ -0,0 +1,114 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+import "./slider.css";
+
+const Slider = () => {
+  return (
+    <div>
+      <Carousel
+        autoPlay={true}
+        interval={4000}
+        infiniteLoop={true}
+        transitionTime={1000}
+        showIndicators={false}
+        swipeable={false}
+        showStatus={false}
+        showThumbs={false}
+      >
+        <div className="slider-height w-full">
+          <img
+            className="h-full w-full bg-cover bg-center"
+            style={{ objectFit: "cover" }}
+            src="/assets/slider/sliderImg1.jpg"
+            alt="prod1"
+          />
+          <div className="w-full h-full main-dark-bg bg-black opacity-30"></div>
+          <div className="main-text bg-transparent text-grey-300">
+            <p className="text-white text-left md:text-2xl bg-transparent mb-2">
+              Trusted & Reliable
+            </p>
+            <h1 className=" text-white text-left font-semibold text-3xl md:text-6xl bg-transparent mb-4">
+              Better Healthcare
+              <br /> for the World
+            </h1>
+            <p className=" text-white text-left md:text-2xl bg-transparent mb-4">
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+              officia deserunt mollit anim id est laborum.
+            </p>
+            <div className="flex justify-start">
+              <Link
+                to="/appointment"
+                className="text-white py-3 px-4 uppercase font-light bg-blue-600 hover:bg-green-400 md:text-xl mb-2"
+              >
+                Book Appointment
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        <div className="slider-height w-full">
+          <img
+            className="h-full w-full bg-cover bg-center"
+            style={{ objectFit: "cover" }}
+            src="/assets/slider/sliderImg2.jpg"
+            alt="prod1"
+          />
+          <div className="w-full h-full main-dark-bg bg-black opacity-30"></div>
+          <div className="main-text-alt bg-transparent text-grey-300">
+            <h1 className=" text-white text-left font-semibold text-3xl md:text-6xl bg-transparent mb-4">
+              Committed to Your <br />
+              Better Health
+            </h1>
+            <p className=" text-white text-left md:text-2xl bg-transparent mb-4">
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+              officia deserunt mollit anim id est laborum.
+            </p>
+            <div className="flex justify-start">
+              <Link
+                to="/doctors"
+                className="text-white py-3 px-4 uppercase font-light bg-blue-600 hover:bg-green-400 md:text-xl mb-2"
+              >
+                Find a Doctor
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        <div className="slider-height w-full">
+          <img
+            className="h-full w-full bg-cover bg-center"
+            style={{ objectFit: "cover" }}
+            src="/assets/slider/sliderImg3.jpg"
+            alt="prod1"
+          />
+          <div className="w-full h-full main-dark-bg bg-black opacity-30"></div>
+          <div className="main-text bg-transparent text-grey-300">
+            <p className="text-white text-left md:text-2xl bg-transparent mb-2">
+              Trusted & Reliable
+            </p>
+            <h1 className=" text-white text-left font-semibold text-3xl md:text-6xl bg-transparent mb-4">
+              Your Trust is
+              <br /> Our Strength
+            </h1>
+            <p className=" text-white text-left md:text-2xl bg-transparent mb-4">
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+              officia deserunt mollit anim id est laborum.
+            </p>
+            <div className="flex justify-start">
+              <Link
+                to="/schedule"
+                className="text-white py-3 px-4 uppercase font-light bg-blue-600 hover:bg-green-400 md:text-xl mb-2"
+              >
+                Visitor Guide
+              </Link>
+            </div>
+          </div>
+        </div>
+      </Carousel>
+    </div>
+  );
+};
+
+export default Slider;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Slider from "../components/HomeComponents/Slider"
 
 const Home = () => {
     return (
         <div>
-            Home Page
+            <Slider />
         </div>
     )
 }
