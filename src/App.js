d4f6db0ebf6c67bf407871ee03041f07df83a001
manishdixit <manishcstechnical@gmail.com>
2021-12-09 19:53:44 +0530

    home component and departments

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
-import Home from "./pages/Home"
+// import Home from "./pages/Home"
+import Home from "./components/Home/Home";
 
 function App() {
   return (
---
+++ src/components/Home/Department/Department.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import './department.css'
+
+function Department() {
+    return (
+        <div className='department'>
+            <div className='depMain'>
+                <h1 className='depMainHead'>Hospital Departments</h1>
+                <p className='depMainPara'>There live the blind texts separated they right at the coast of the Semantics.</p>
+                <div className='depLine'></div>
+            </div>
+            <div className='depContainer'>
+                <div className='depCard'>
+                    <div className='depIcon'></div>
+                    <h1 className='depHead'>Surgery</h1>
+                    <h3 className='depAbout'>Operations & surgeries</h3>
+                    <p className='depDetail'>Behind the word mountains, far from the countries Vokalia.</p>
+                    <a href="https://www.google.com" className='depLink'>Get Details..</a>
+                </div>
+
+                <div className='depCard'>
+                    <div className='depIcon'></div>
+                    <h1 className='depHead'>Surgery</h1>
+                    <h3 className='depAbout'>Operations & surgeries</h3>
+                    <p className='depDetail'>Behind the word mountains, far from the countries Vokalia.</p>
+                    <a href="https://www.google.com" className='depLink'>Get Details..</a>
+                </div>
+
+                <div className='depCard'>
+                    <div className='depIcon'></div>
+                    <h1 className='depHead'>Surgery</h1>
+                    <h3 className='depAbout'>Operations & surgeries</h3>
+                    <p className='depDetail'>Behind the word mountains, far from the countries Vokalia.</p>
+                    <a href="https://www.google.com" className='depLink'>Get Details..</a>
+                </div>
+
+                <div className='depCard'>
+                    <div className='depIcon'></div>
+                    <h1 className='depHead'>Surgery</h1>
+                    <h3 className='depAbout'>Operations & surgeries</h3>
+                    <p className='depDetail'>Behind the word mountains, far from the countries Vokalia.</p>
+                    <a href="https://www.google.com" className='depLink'>Get Details..</a>
+                </div>
+
+                <div className='depCard'>
+                    <div className='depIcon'></div>
+                    <h1 className='depHead'>Surgery</h1>
+                    <h3 className='depAbout'>Operations & surgeries</h3>
+                    <p className='depDetail'>Behind the word mountains, far from the countries Vokalia.</p>
+                    <a href="https://www.google.com" className='depLink'>Get Details..</a>
+                </div>
+
+                <div className='depCard'>
+                    <div className='depIcon'></div>
+                    <h1 className='depHead'>Surgery</h1>
+                    <h3 className='depAbout'>Operations & surgeries</h3>
+                    <p className='depDetail'>Behind the word mountains, far from the countries Vokalia.</p>
+                    <a href="https://www.google.com" className='depLink'>Get Details..</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Department
