541bf26ca75bf88ee9a47d082b04b1bcaef78b37
Chirag Khatri <kchiragkhatri2001@gmail.com>
2021-12-10 20:26:33 +0530

    testimonials done

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,6 +3,7 @@ import AboutHead from "../components/About/AboutHead";
 import BrandSlider from "../components/About/BrandSlider";
 import WhyChooseUs from "../components/About/WhyChooseUs";
 import AboutFacts from "../components/About/AboutFacts";
+import Testimonials from "../components/About/Testimonials";
 // import { Link } from "react-router-dom";
 import "../components/About/About.css";
 
@@ -73,6 +74,7 @@ const About = () => {
       {/*About Content Closed */}
       <AboutFacts />
       <WhyChooseUs />
+      <Testimonials/>
       <BrandSlider />
     </>
   );
