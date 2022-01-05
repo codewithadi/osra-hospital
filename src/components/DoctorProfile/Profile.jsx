1828920fed9cc2596a6967df598a31410b4cd86b
Chirag Khatri <kchiragkhatri2001@gmail.com>
2022-01-05 19:09:12 +0530

    reponsiveness added

--- src/components/DoctorProfile/Profile.jsx
+++ src/components/DoctorProfile/Profile.jsx
@@ -19,8 +19,8 @@ const Profile = () => {
       <div className="depDetail">
         <div className="depDetailContainer">
           <div className="drTextHead">
-            <div className="drDetailAbout8">
-              <div className="my-24 ml-10">
+            <div className="drDetailAbout">
+              <div className="my-24 ml-4 md:ml-10">
                 <div className="my-2 flex">
                   <img className="w-7 h-6" src="/assets/dr/map.png" />
                   <h4 className="text-lg mx-1">Egyptian</h4>
@@ -28,15 +28,16 @@ const Profile = () => {
                 <h2 className="text-4xl font-semibold">Dr. Eman Radwan</h2>
                 <h4 className="mt-4">Neuro Psychiatric Specialist</h4>
                 <div className="mt-20">
-                  <h5 className="flex">
-                    Qualification :
-                    <p className="ml-1">
+                  <span className="md:flex">
+                    <h5 className="font-semibold">Qualification :</h5>
+                    <p className="ml-1 mb-2 md:mb-0">
                       Master's degree in Psychiatry and neurology
                     </p>
-                  </h5>
-                  <h4 className="flex">
-                    Years of experience : <p className="ml-1">14 years</p>
-                  </h4>
+                  </span>
+                  <span className="md:flex mt-2">
+                    <h5 className="font-semibold">Years of experience : </h5>
+                    <p className="ml-1">14 years</p>
+                  </span>
                 </div>
                 <button className="border-white border-2 px-4 py-2 rounded-full mt-4">
                   <Link to="/appointment"> Book An Appointment</Link>
