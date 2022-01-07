e96607ac2d276b6b4ddaead031c5a366fed59c97
ajfuturistics <ajfuturistics@gmail.com>
2022-01-07 20:24:41 +0530

    updated footer and nav bar

--- src/components/Services/ServiceGrid.jsx
+++ src/components/Services/ServiceGrid.jsx
@@ -11,7 +11,7 @@ const ServiceGrid = ({ imgUrl, title, linkUrl }) => {
             <div className="w-full">
                 <img className="w-full h-auto" src={imgUrl} alt="service" />
             </div>
-            <div className="w-full flex flex-col justify-center pl-2 md:pl-5 pr-2 py-2 md:py-3">
+            <div className="w-full flex flex-col justify-center pl-2 md:pl-5 pr-2 py-2 md:py-3 text-center">
                 <h2
                     className="text-base md:text-2xl mb-3 md:mb-5"
                     style={{ color: "#3c0c1c" }}
@@ -21,7 +21,7 @@ const ServiceGrid = ({ imgUrl, title, linkUrl }) => {
                 <Link
                     onClick={scrollToTop}
                     to={linkUrl}
-                    className="text-sm md:text-lg text-white py-3 px-4 font-light  bg-green-900 hover:bg-green-700 mb-2"
+                    className="text-sm md:text-lg text-white mx-4 py-3 px-4 font-light  bg-green-900 hover:bg-green-700 mb-2"
                 >
                     Learn More
                 </Link>
--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -4,8 +4,9 @@ const TopBar = () => {
     return (
         <div className="w-full h-7 flex justify-end items-center px-3 md:px-6 z-100 shadow-md">
             <a
-                className="p-1 md:py-1 md:px-6 mr-4 bg-blue-600 text-sm md:text-base text-white font-semibold uppercase"
+                className="p-1 md:py-1 md:px-6 mr-4 text-sm md:text-base text-white font-semibold uppercase"
                 href="tel:011431111"
+                style={{ background: "#005C00" }}
             >
                 Emergency: 011-431-111
             </a>
