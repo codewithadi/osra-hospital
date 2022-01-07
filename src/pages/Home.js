9e9c6eb944185a55e09b56fa905f33cb5ce4907f
Chirag Khatri <kchiragkhatri2001@gmail.com>
2022-01-07 13:19:27 +0530

    slider added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,22 @@
 import React from "react";
 import Slider from "../components/Home/Slider/Slider";
-import Department from "../components/Home/Department/Department";
+// import Department from "../components/Home/Department/Department";
 // import FindSection from "../components/Home/FindSection/FindSection";
 // import Stories from "../components/Home/Stories";
 import SearchDoctor from "../components/Home/SearchDoctor/SearchDoctor";
+import Dslider from "../components/Home/DepartmentSlider/Dslider";
 import Facts from "../components/Home/Facts";
 
 const Home = () => {
   return (
     <div>
-        <Slider />
-        <SearchDoctor />
-        {/* <FindSection /> */}
-        <Facts/>
-        <Department />
-        {/* <Stories/> */}
+      <Slider />
+      <SearchDoctor />
+      {/* <FindSection /> */}
+      <Facts />
+      <Dslider />
+      {/* <Department /> */}
+      {/* <Stories/> */}
     </div>
   );
 };
