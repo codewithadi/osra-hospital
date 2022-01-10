ce844f3a7a3acf56a4cda74842fa5b993369390b
ajfuturistics <ajfuturistics@gmail.com>
2022-01-10 17:40:56 +0530

    fixed some bugs

--- src/components/Services/AllClinics/AllClinics.js
+++ src/components/Services/AllClinics/AllClinics.js
@@ -1,10 +1,9 @@
 import React from "react";
-import { Link, Navigate, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { demoClinic } from "../../DemoData/democlinic";
 import "./department.css";
 
 function AllClinics() {
-    const navigate = useNavigate();
     const scrollToTop = () => {
         window.scrollTo(0, 0);
     };
