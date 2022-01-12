eb42976278562fc331db435273e4f8172d2009bf
Chirag Khatri <kchiragkhatri2001@gmail.com>
2022-01-12 16:28:11 +0530

    urology

--- src/components/Departments/Urology.js
+++ src/components/Departments/Urology.js
@@ -2,180 +2,160 @@ import React from "react";
 import "./departments.css";
 import { demoData } from "../DemoData/demodata";
 import SingleDoctor from "../Doctor/SingleDoctor";
-const treatmentsOffered = {
+import { withTranslation } from "react-i18next";
+
+function Urology({ t }) {
+  const treatmentsOffered = {
     DiagnosticModalities: [
-        {
-            name: "Urethro - cystocopy",
-            detail: "",
-            logo: "",
-        },
-        {
-            name: "Diagnostic Laparoscopy",
-            detail: "",
-            logo: "",
-        },
+      {
+        name: `${t("Urology.dm1")}`,
+        detail: "",
+        logo: "",
+      },
+      {
+        name: `${t("Urology.dm2")}`,
+        detail: "",
+        logo: "",
+      },
     ],
     TherapeuticModalities: [
-        {
-            name: "Pyelolithotomy",
-            detail: "",
-            logo: "",
-        },
-        {
-            name: "ESWL",
-            detail: "",
-            logo: "",
-        },
-        {
-            name: "Andrology",
-            detail: "",
-            logo: "",
-        },
-        {
-            name: "Laproscopic Procedures",
-            detail: "",
-            logo: "",
-        },
-        {
-            name: "Folley Catherization",
-            detail: "",
-            logo: "",
-        },
+      {
+        name: `${t("Urology.tm1")}`,
+        detail: "",
+        logo: "",
+      },
+      {
+        name: `${t("Urology.tm2")}`,
+        detail: "",
+        logo: "",
+      },
+      {
+        name: `${t("Urology.tm3")}`,
+        detail: "",
+        logo: "",
+      },
+      {
+        name: `${t("Urology.tm4")}`,
+        detail: "",
+        logo: "",
+      },
+      {
+        name: `${t("Urology.tm5")}`,
+        detail: "",
+        logo: "",
+      },
     ],
-};
+  };
+  const filterdDoc = demoData.filter((data) =>
+    data.department.includes("urology")
+  );
 
-function Urology() {
-    const filterdDoc = demoData.filter((data) =>
-        data.department.includes("urology")
-    );
+  return (
+    <div className="departments">
+      <div className="depMainBack">
+        <img
+          className="depMainImg"
+          src="/assets/department/depMain.jpg"
+          alt="department Back"
+        />
+        <div className="depMainOverlay">
+          <h1 className="depMainText">{t("Urology.dname")}</h1>
+        </div>
+      </div>
 
-    return (
-        <div className="departments">
-            <div className="depMainBack">
-                <img
-                    className="depMainImg"
-                    src="/assets/department/depMain.jpg"
-                    alt="department Back"
-                />
-                <div className="depMainOverlay">
-                    <h1 className="depMainText">Urology</h1>
-                </div>
+      <div className="depDetail">
+        <div className="depDetailContainer">
+          <div className="depTextHead">
+            <h1 className="depDetailTitle">{t("Urology.dname")}</h1>
+            <div className="depDetailAbout">
+            {t("Urology.detail")}
             </div>
-
-            <div className="depDetail">
-                <div className="depDetailContainer">
-                    <div className="depTextHead">
-                        <h1 className="depDetailTitle">Urology Department</h1>
-                        <div className="depDetailAbout">
-                            Urology is a part of health care that deals with
-                            diseases of the male and female urinary tract
-                            (kidneys, ureters, bladder and urethra). It also
-                            deals with the male organs that are able to make
-                            babies (penis, testes, scrotum, prostate, etc.).
-                            Since health problems in these body parts can happen
-                            to everyone, urologic health is important.
-                        </div>
-                        {/* <p className="depDetailPara">
+            {/* <p className="depDetailPara">
                             The scope of organs under Urology include kidneys,
                             adrenal glands, ureters, urinary bladder, and
                             urethra.
                         </p> */}
-                    </div>
-                    <div className="depImgWrapper">
-                        <img
-                            className="depDetailImg"
-                            src="/assets/images/urology.jpg"
-                            alt="Urology"
-                        />
-                    </div>
-                </div>
-            </div>
+          </div>
+          <div className="depImgWrapper">
+            <img
+              className="depDetailImg"
+              src="/assets/images/urology.jpg"
+              alt="Urology"
+            />
+          </div>
+        </div>
+      </div>
 
-            <div className="treatmentDetails">
-                <div className="depMain">
-                    <h1 className="depMainHead">Treatments Offered</h1>
-                    <p className="depMainPara">
-                        Due to the multi-faceted nature of urology, our
-                        physicians also boast exceptional knowledge in
-                    </p>
-                    <div className="depLine"></div>
-                </div>
+      <div className="treatmentDetails">
+        <div className="depMain">
+          <h1 className="depMainHead">{t("to")}</h1>
+          <p className="depMainPara">
+          {t("Urology.treatment")}
+          </p>
+          <div className="depLine"></div>
+        </div>
 
-                <div className="treatmentWrapper">
-                    <h1 className="depMainHead">Diagnostic Modalities :</h1>
+        <div className="treatmentWrapper">
+          <h1 className="depMainHead">{t("dm")}</h1>
+        </div>
+        <div className="treatmentWrapper">
+          {treatmentsOffered.DiagnosticModalities.map((item, index) => (
+            <>
+              <div className="treatmentCard" key={index}>
+                <div className="checkLogo">
+                  <img src="/assets/dr/customcheck.jpg" alt="check" />
                 </div>
-                <div className="treatmentWrapper">
-                    {treatmentsOffered.DiagnosticModalities.map(
-                        (item, index) => (
-                            <>
-                                <div className="treatmentCard" key={index}>
-                                    <div className="checkLogo">
-                                        <img
-                                            src="/assets/dr/customcheck.jpg"
-                                            alt="check"
-                                        />
-                                    </div>
-                                    <div className="treatmentText">
-                                        <h2 className="treatmentTitle">
-                                            {item.name}
-                                        </h2>
-                                    </div>
-                                </div>
-                            </>
-                        )
-                    )}
+                <div className="treatmentText">
+                  <h2 className="treatmentTitle">{item.name}</h2>
                 </div>
+              </div>
+            </>
+          ))}
+        </div>
 
-                <div className="treatmentWrapper">
-                    <h1 className="depMainHead">Therapeutic Modalities :</h1>
+        <div className="treatmentWrapper">
+          <h1 className="depMainHead">{t("tm")}</h1>
+        </div>
+        <div className="treatmentWrapper">
+          {treatmentsOffered.TherapeuticModalities.map((item, index) => (
+            <>
+              <div className="treatmentCard" key={index}>
+                <div className="checkLogo">
+                  <img src="/assets/dr/customcheck.jpg" alt="check" />
                 </div>
-                <div className="treatmentWrapper">
-                    {treatmentsOffered.TherapeuticModalities.map(
-                        (item, index) => (
-                            <>
-                                <div className="treatmentCard" key={index}>
-                                    <div className="checkLogo">
-                                        <img
-                                            src="/assets/dr/customcheck.jpg"
-                                            alt="check"
-                                        />
-                                    </div>
-                                    <div className="treatmentText">
-                                        <h2 className="treatmentTitle">
-                                            {item.name}
-                                        </h2>
-                                    </div>
-                                </div>
-                            </>
-                        )
-                    )}
+                <div className="treatmentText">
+                  <h2 className="treatmentTitle">{item.name}</h2>
                 </div>
-            </div>
+              </div>
+            </>
+          ))}
+        </div>
+      </div>
 
-            <div className="depDoctors">
-                <div className="depMain">
-                    <h1 className="depMainHead">Department Doctors</h1>
-                    <p className="depMainPara">
-                        Physicians at Osra Hospital have excellent knowledge
-                    </p>
-                    <div className="depLine"></div>
-                </div>
-                <div className="grid grid-cols-1 justify-center justify-items-center items-center gap-4 md:grid-cols-4 px-2 md:px-10">
-                    {filterdDoc.map((doc, index) => (
-                        <SingleDoctor
-                            key={index}
-                            imgUrl={doc.imgUrl}
-                            name={doc.name}
-                            position={doc.position}
-                            mobno={doc.mobno}
-                            email={doc.email}
-                            linkto={doc.linkto}
-                        />
-                    ))}
-                </div>
-            </div>
+      <div className="depDoctors">
+        <div className="depMain">
+          <h1 className="depMainHead">Department Doctors</h1>
+          <p className="depMainPara">
+            Physicians at Osra Hospital have excellent knowledge
+          </p>
+          <div className="depLine"></div>
+        </div>
+        <div className="grid grid-cols-1 justify-center justify-items-center items-center gap-4 md:grid-cols-4 px-2 md:px-10">
+          {filterdDoc.map((doc, index) => (
+            <SingleDoctor
+              key={index}
+              imgUrl={doc.imgUrl}
+              name={doc.name}
+              position={doc.position}
+              mobno={doc.mobno}
+              email={doc.email}
+              linkto={doc.linkto}
+            />
+          ))}
+        </div>
+      </div>
 
-            {/* <div className="depBanner">
+      {/* <div className="depBanner">
                 <img
                     class="depBannerImg"
                     src="/assets/department/depLastBack.jpg"
@@ -198,8 +178,8 @@ function Urology() {
                     </div>
                 </div>
             </div> */}
-        </div>
-    );
+    </div>
+  );
 }
 
-export default Urology;
+export default withTranslation()(Urology);
