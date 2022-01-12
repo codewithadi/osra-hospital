8de061217db1b3280a75d7ea4d084babb5790459
Chirag Khatri <kchiragkhatri2001@gmail.com>
2022-01-12 14:24:12 +0530

    cardio added

--- src/components/Departments/Cardiologist.js
+++ src/components/Departments/Cardiologist.js
@@ -2,176 +2,156 @@ import React from "react";
 import "./departments.css";
 import { demoData } from "../DemoData/demodata";
 import SingleDoctor from "../Doctor/SingleDoctor";
+import { withTranslation } from "react-i18next";
 
+function Cardiologist({t}) {
+  const treatmentsOffered = [
+    {
+      name: `${t("CardiologyDepartment.tm1")}`,
+      detail: "",
+      logo: "",
+    },
+    {
+      name: `${t("CardiologyDepartment.tm2")}`,
+      detail: "",
+      logo: "",
+    },
+    {
+      name: `${t("CardiologyDepartment.tm3")}`,
+      detail: "",
+      logo: "",
+    },
+    {
+      name: `${t("CardiologyDepartment.tm4")}`,
+      detail: "",
+      logo: "",
+    },
+    {
+      name: `${t("CardiologyDepartment.tm5")}`,
+      detail: "",
+      logo: "",
+    },
+    {
+      name: `${t("CardiologyDepartment.tm6")}`,
+      detail: "",
+      logo: "",
+    },
+    {
+      name: `${t("CardiologyDepartment.tm7")}`,
+      detail: "",
+      logo: "",
+    },
+    {
+      name: `${t("CardiologyDepartment.tm8")}`,
+      detail: "",
+      logo: "",
+    },
+    {
+      name: `${t("CardiologyDepartment.tm9")}`,
+      detail: "",
+      logo: "",
+    },
+    {
+      name: `${t("CardiologyDepartment.tm10")}`,
+      detail: "",
+      logo: "",
+    },
+  ];
 
+  const filterdDoc = demoData.filter((data) =>
+    data.department.includes("cardiologist")
+  );
 
-function Cardiologist() {
-    const treatmentsOffered = [
-        {
-            name: "Internal medicine Cases",
-            detail: "",
-            logo: "",
-        },
-        {
-            name: "Cardio Vascular System",
-            detail: "",
-            logo: "",
-        },
-        {
-            name: "Trauma",
-            detail: "",
-            logo: "",
-        },
-        {
-            name: "High Blood Pressure",
-            detail: "",
-            logo: "",
-        },
-        {
-            name: "Diabetes",
-            detail: "",
-            logo: "",
-        },
-        {
-            name: "Incision biopsy",
-            detail: "",
-            logo: "",
-        },
-        {
-            name: "Excision Biopsy",
-            detail: "",
-            logo: "",
-        },
-        {
-            name: "Diagnostic Laparoscopy",
-            detail: "",
-            logo: "",
-        },
-        {
-            name: "Respiratory system",
-            detail: "",
-            logo: "",
-        },
-        {
-            name: "Digestive system",
-            detail: "",
-            logo: "",
-        },
-    ];
-    
-    const filterdDoc = demoData.filter((data) =>
-        data.department.includes("cardiologist")
-    );
+  return (
+    <div className="departments">
+      <div className="depMainBack">
+        <img
+          className="depMainImg"
+          src="/assets/department/3-min.png"
+          alt="department Back"
+        />
+        <div className="depMainOverlay">
+          <h1 className="depMainText">{t("CardiologyDepartment.dname")}</h1>
+        </div>
+      </div>
 
-    return (
-        <div className="departments">
-            <div className="depMainBack">
-                <img
-                    className="depMainImg"
-                    src="/assets/department/3-min.png"
-                    alt="department Back"
-                />
-                <div className="depMainOverlay">
-                    <h1 className="depMainText">Cardiology</h1>
-                </div>
+      <div className="depDetail">
+        <div className="depDetailContainer">
+          <div className="depTextHead">
+            <h1 className="depDetailTitle">{t("CardiologyDepartment.dname")}</h1>
+            <div className="depDetailAbout">
+            {t("CardiologyDepartment.detail")}
             </div>
+          </div>
+          <div className="depImgWrapper">
+            <img
+              className="depDetailImg"
+              src="/assets/department/Cardiology.jpg"
+              alt="earnose"
+            />
+          </div>
+        </div>
+      </div>
 
-            <div className="depDetail">
-                <div className="depDetailContainer">
-                    <div className="depTextHead">
-                        <h1 className="depDetailTitle">
-                            Cardiology Department
-                        </h1>
-                        <div className="depDetailAbout">
-                            Cardiology is the study and treatment of disorders
-                            of the heart and the blood vessels. A person with
-                            heart disease or cardiovascular disease may be
-                            referred to a cardiologist. Cardiology is a branch
-                            of internal medicine.
-                            <br />
-                            <br />A cardiologist is not the same as a cardiac
-                            surgeon. A cardiac surgeon opens the chest and
-                            performs heart surgery. A cardiologist specializes
-                            in diagnosing and treating diseases of the
-                            cardiovascular system.
-                        </div>
-                    </div>
-                    <div className="depImgWrapper">
-                        <img
-                            className="depDetailImg"
-                            src="/assets/department/Cardiology.jpg"
-                            alt="earnose"
-                        />
-                    </div>
-                </div>
-            </div>
+      <div className="treatmentDetails">
+        <div className="depMain">
+          <h1 className="depMainHead">{t("to")}</h1>
+          <p className="depMainPara">
+          {t("CardiologyDepartment.treatment")}
+          </p>
+          <div className="depLine"></div>
+        </div>
 
-            <div className="treatmentDetails">
-                <div className="depMain">
-                    <h1 className="depMainHead">Treatments Offered</h1>
-                    <p className="depMainPara">
-                        Our department of Cardiology is dedicated to exceeding
-                        expectations. We provide cutting edge treatment of
-                        disorders below
-                    </p>
-                    <div className="depLine"></div>
-                </div>
-
-                <div className="treatmentWrapper">
-                    {treatmentsOffered.map((item, index) => (
-                        <>
-                            <div className="treatmentCard" key={index}>
-                                {/* for logo uncomment n remove check */}
-                                {/* <div className="treatmentLogo">
+        <div className="treatmentWrapper">
+          {treatmentsOffered.map((item, index) => (
+            <>
+              <div className="treatmentCard" key={index}>
+                {/* for logo uncomment n remove check */}
+                {/* <div className="treatmentLogo">
                   <img src="/assets/department/surgery.png" alt="anyimg" />
                 </div> */}
-                                <div className="checkLogo">
-                                    <img
-                                        src="/assets/dr/customcheck.jpg"
-                                        alt="check"
-                                    />
-                                </div>
-                                <div className="treatmentText">
-                                    <h2 className="treatmentTitle">
-                                        {item.name}
-                                    </h2>
-                                    {/* for content  */}
-                                    {/* <p className="treatmentAbout">
+                <div className="checkLogo">
+                  <img src="/assets/dr/customcheck.jpg" alt="check" />
+                </div>
+                <div className="treatmentText">
+                  <h2 className="treatmentTitle">{item.name}</h2>
+                  {/* for content  */}
+                  {/* <p className="treatmentAbout">
                     Dunt in culpa qui officia deserunt mollit anim id est
                     laborum.
                   </p> */}
-                                </div>
-                            </div>
-                        </>
-                    ))}
                 </div>
-            </div>
+              </div>
+            </>
+          ))}
+        </div>
+      </div>
 
-            <div className="depDoctors">
-                <div className="depMain">
-                    <h1 className="depMainHead">Department Doctors</h1>
-                    <p className="depMainPara">
-                        There live the blind texts separated they right at the
-                        coast of the Semantics.
-                    </p>
-                    <div className="depLine"></div>
-                </div>
-                <div className="grid grid-cols-1 justify-center justify-items-center items-center gap-4 md:grid-cols-4 px-2 md:px-10">
-                    {filterdDoc.map((doc, index) => (
-                        <SingleDoctor
-                            key={index}
-                            imgUrl={doc.imgUrl}
-                            name={doc.name}
-                            position={doc.position}
-                            mobno={doc.mobno}
-                            email={doc.email}
-                            linkto={doc.linkto}
-                        />
-                    ))}
-                </div>
-            </div>
+      <div className="depDoctors">
+        <div className="depMain">
+          <h1 className="depMainHead">Department Doctors</h1>
+          <p className="depMainPara">
+            There live the blind texts separated they right at the coast of the
+            Semantics.
+          </p>
+          <div className="depLine"></div>
+        </div>
+        <div className="grid grid-cols-1 justify-center justify-items-center items-center gap-4 md:grid-cols-4 px-2 md:px-10">
+          {filterdDoc.map((doc, index) => (
+            <SingleDoctor
+              key={index}
+              imgUrl={doc.imgUrl}
+              name={doc.name}
+              position={doc.position}
+              mobno={doc.mobno}
+              email={doc.email}
+              linkto={doc.linkto}
+            />
+          ))}
+        </div>
+      </div>
 
-            {/* <div className="depBanner">
+      {/* <div className="depBanner">
         <img
           class="depBannerImg"
           src="/assets/department/depLastBack.jpg"
@@ -192,8 +172,8 @@ function Cardiologist() {
           </div>
         </div>
       </div> */}
-        </div>
-    );
+    </div>
+  );
 }
 
-export default Cardiologist;
+export default withTranslation()(Cardiologist);
--- src/components/Departments/Gastroenterology .jsx
+++ src/components/Departments/Gastroenterology .jsx
@@ -2,147 +2,126 @@ import React from "react";
 import "./departments.css";
 import { demoData } from "../DemoData/demodata";
 import SingleDoctor from "../Doctor/SingleDoctor";
-const treatmentsOffered = [
-    {
-        name: "Herpetology and Endoscopy",
-        detail: "",
-        logo: "",
-    },
-    {
-        name: "Diagnose and treating internal Medicine",
-        detail: "",
-        logo: "",
-    },
-    {
-        name: "Liver Diseases",
-        detail: "",
-        logo: "",
-    },
-    {
-        name: "Gastrointestinal",
-        detail: "",
-        logo: "",
-    },
-    {
-        name: "Pancreatic Diseases",
-        detail: "",
-        logo: "",
-    },
-];
+import { withTranslation } from "react-i18next";
 
-function Gastroenterology() {
-    const filterdDoc = demoData.filter((data) =>
-        data.department.includes("gastroenterology")
-    );
-    return (
-        <div className="departments">
-            <div className="depMainBack">
-                <img
-                    className="depMainImg"
-                    src="/assets/department/9-min.png"
-                    alt="department Back"
-                />
-                <div className="depMainOverlay">
-                    <h1 className="depMainText">Gastroenterology</h1>
-                </div>
+function Gastroenterology({t}) {
+  const treatmentsOffered = [
+    {
+      name: `${t("GastroenterologyDepartment.tm1")}`,
+      detail: "",
+      logo: "",
+    },
+    {
+      name: `${t("GastroenterologyDepartment.tm2")}`,
+      detail: "",
+      logo: "",
+    },
+    {
+      name: `${t("GastroenterologyDepartment.tm3")}`,
+      detail: "",
+      logo: "",
+    },
+    {
+      name: `${t("GastroenterologyDepartment.tm4")}`,
+      detail: "",
+      logo: "",
+    },
+    {
+      name: `${t("GastroenterologyDepartment.tm5")}`,
+      detail: "",
+      logo: "",
+    },
+  ];
+
+  const filterdDoc = demoData.filter((data) =>
+    data.department.includes("gastroenterology")
+  );
+  return (
+    <div className="departments">
+      <div className="depMainBack">
+        <img
+          className="depMainImg"
+          src="/assets/department/9-min.png"
+          alt="department Back"
+        />
+        <div className="depMainOverlay">
+          <h1 className="depMainText">{t("GastroenterologyDepartment.dname")}</h1>
+        </div>
+      </div>
+
+      <div className="depDetail">
+        <div className="depDetailContainer">
+          <div className="depTextHead">
+            <h1 className="depDetailTitle">{t("GastroenterologyDepartment.dname")}</h1>
+            <div className="depDetailAbout">
+            {t("GastroenterologyDepartment.detail")}
             </div>
+          </div>
+          <div className="depImgWrapper">
+            <img
+              className="depDetailImg"
+              src="/assets/images/gastroenterology.jpg"
+              alt="Gastroenterology"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="treatmentDetails">
+        <div className="depMain">
+          <h1 className="depMainHead">{t("to")}</h1>
+          <p className="depMainPara">
+          {t("GastroenterologyDepartment.treatment")}
+          </p>
+          <div className="depLine"></div>
+        </div>
 
-            <div className="depDetail">
-                <div className="depDetailContainer">
-                    <div className="depTextHead">
-                        <h1 className="depDetailTitle">
-                            Gastroenterology Department
-                        </h1>
-                        <div className="depDetailAbout">
-                            Gastroenterology is a medical specialty dealing with
-                            the study of the digestive system and its disorders.
-                            ... Gastrointestinal services incorporate a joint
-                            medical and surgical approach for the prevention,
-                            diagnosis and treatment of gastrointestinal, liver
-                            and pancreatic disorders including cancer.
-                            <br />
-                            <br />
-                            The department is manned by esteemed and experienced
-                            gastroenterologists, efficient medical officers,
-                            skilled and compassionate paramedical staff, and
-                            well-trained technicians. The aim of the department
-                            is to provide a patient-focused approach.
-                        </div>
-                    </div>
-                    <div className="depImgWrapper">
-                        <img
-                            className="depDetailImg"
-                            src="/assets/images/gastroenterology.jpg"
-                            alt="Gastroenterology"
-                        />
-                    </div>
-                </div>
-            </div>
-            <div className="treatmentDetails">
-                <div className="depMain">
-                    <h1 className="depMainHead">Treatments Offered</h1>
-                    <p className="depMainPara">
-                        Our Gastroenterology Department is dedicated to
-                        exceeding expectations. We provide cutting edge
-                        treatment
-                    </p>
-                    <div className="depLine"></div>
-                </div>
-
-                <div className="treatmentWrapper">
-                    {treatmentsOffered.map((item, index) => (
-                        <>
-                            <div className="treatmentCard" key={index}>
-                                {/* for logo uncomment n remove check */}
-                                {/* <div className="treatmentLogo">
+        <div className="treatmentWrapper">
+          {treatmentsOffered.map((item, index) => (
+            <>
+              <div className="treatmentCard" key={index}>
+                {/* for logo uncomment n remove check */}
+                {/* <div className="treatmentLogo">
                   <img src="/assets/department/surgery.png" alt="anyimg" />
                 </div> */}
-                                <div className="checkLogo">
-                                    <img
-                                        src="/assets/dr/customcheck.jpg"
-                                        alt="check"
-                                    />
-                                </div>
-                                <div className="treatmentText">
-                                    <h2 className="treatmentTitle">
-                                        {item.name}
-                                    </h2>
-                                    {/* for content  */}
-                                    {/* <p className="treatmentAbout">
+                <div className="checkLogo">
+                  <img src="/assets/dr/customcheck.jpg" alt="check" />
+                </div>
+                <div className="treatmentText">
+                  <h2 className="treatmentTitle">{item.name}</h2>
+                  {/* for content  */}
+                  {/* <p className="treatmentAbout">
                     Dunt in culpa qui officia deserunt mollit anim id est
                     laborum.
                   </p> */}
-                                </div>
-                            </div>
-                        </>
-                    ))}
                 </div>
-            </div>
+              </div>
+            </>
+          ))}
+        </div>
+      </div>
 
-            <div className="depDoctors">
-                <div className="depMain">
-                    <h1 className="depMainHead">Department Doctors</h1>
-                    <p className="depMainPara">
-                        Doctors in Gastroenterology are
-                    </p>
-                    <div className="depLine"></div>
-                </div>
-                <div className="grid grid-cols-1 justify-center justify-items-center items-center gap-4 md:grid-cols-4 px-2 md:px-10">
-                    {filterdDoc.map((doc, index) => (
-                        <SingleDoctor
-                            key={index}
-                            imgUrl={doc.imgUrl}
-                            name={doc.name}
-                            position={doc.position}
-                            mobno={doc.mobno}
-                            email={doc.email}
-                            linkto={doc.linkto}
-                        />
-                    ))}
-                </div>
-            </div>
+      <div className="depDoctors">
+        <div className="depMain">
+          <h1 className="depMainHead">Department Doctors</h1>
+          <p className="depMainPara">Doctors in Gastroenterology are</p>
+          <div className="depLine"></div>
+        </div>
+        <div className="grid grid-cols-1 justify-center justify-items-center items-center gap-4 md:grid-cols-4 px-2 md:px-10">
+          {filterdDoc.map((doc, index) => (
+            <SingleDoctor
+              key={index}
+              imgUrl={doc.imgUrl}
+              name={doc.name}
+              position={doc.position}
+              mobno={doc.mobno}
+              email={doc.email}
+              linkto={doc.linkto}
+            />
+          ))}
+        </div>
+      </div>
 
-            {/* <div className="depBanner">
+      {/* <div className="depBanner">
                 <img
                     class="depBannerImg"
                     src="/assets/department/depLastBack.jpg"
@@ -165,8 +144,8 @@ function Gastroenterology() {
                     </div>
                 </div>
             </div> */}
-        </div>
-    );
+    </div>
+  );
 }
 
-export default Gastroenterology;
+export default withTranslation()(Gastroenterology);
