19c7e78b40bf71e924517275c26c7269f40cf635
Kumari-Ujwala-Sinha <kumariujwalas@gmail.com>
2022-01-13 17:03:36 +0530

    changes

--- src/components/DoctorProfile/Profile.jsx
+++ src/components/DoctorProfile/Profile.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
 import { Link } from "react-router-dom";
 import "../Departments/departments.css";
 import { useLocation } from "react-router-dom";
 import { demoData } from "../DemoData/demodata";
+import { demoDatas } from "../DemoData/demodatas";
+import { withTranslation } from "react-i18next";
+import Loading from "../Loading/Loading";
 
-const Profile = () => {
+const Profile = ({t}) => {
+    const [doctors, setDoctors]=useState()
+    const [loading, setloading] = useState(true);
     //assigning location variable
     const location = useLocation();
     //destructuring pathname from location
@@ -15,11 +20,42 @@ const Profile = () => {
     const singleDoc = demoData.filter(
         (data) => data.linkto === splitLocation[2]
     );
+    const singleDocar = demoDatas.filter(
+        (data) => data.linkto === splitLocation[2]
+    );
+    
+    useEffect(() => {
+        setloading(true);
+        const getDept =  () => {
+           
+            
+            const language=localStorage.getItem("language")
+            console.log(language==="ar")
+            if(language==="ar"){
+                console.log("ar")
+                setDoctors(singleDocar)
+            }else{
+                console.log("en")
+                setDoctors(singleDoc)
+            }
+
+            //setDepartment(demodept);
+           
+
+           
+        };
+        getDept();
+        setloading(false);
+    }, []);
 
     return (
         <>
-            {singleDoc.map((doc) => (
-                <div className="departments">
+ {loading ? (
+                <div className="w-full h-full flex justify-center items-center py-4">
+                    <Loading />
+                </div>
+            ) :(<div> {doctors.map((doc) => (
+                <div className="departments"  >
                     <div className="depDetail">
                         <div className="depProfileDetailContainer">
                             <div className="drTextHead">
@@ -31,43 +67,43 @@ const Profile = () => {
                                                 src="/assets/dr/map.png"
                                                 alt="data"
                                             />
-                                            <h4 className="text-lg mx-1">
+                                            <h4 className="text-lg mx-1" dir={t("directionc")}>
                                                 {doc.Nationality}
                                             </h4>
                                         </div>
-                                        <h2 className="text-4xl font-semibold">
-                                            {doc.name}
+                                        <h2 className="text-4xl font-semibold" dir={t("directionc")}>
+                                            {doc.namear}
                                         </h2>
-                                        <h4 className="mt-4">{doc.position}</h4>
+                                        <h4 className="mt-4" dir={t("directionc")}>{doc.positionar}</h4>
                                         <div className="mt-20">
                                             <h5 className="flex mb-2">
-                                                Qualification :
-                                                <p className="ml-1">
+                                                {t("qualification")} :
+                                                <p className="ml-1" dir={t("directionc")}>
                                                     {doc.Qualification}
                                                 </p>
                                             </h5>
                                             {!doc.License < 1 && (
-                                                <h5 className="flex flex-col mb-2">
-                                                    License :
+                                                <h5 className="flex flex-col mb-2" dir={t("directionc")}>
+                                                   {t("License")} :
                                                     {doc.License.map((lic) => (
                                                         <>
-                                                            <p className="ml-3">
+                                                            <p className="ml-3" dir={t("directionc")}>
                                                                 {lic}
                                                             </p>
                                                         </>
                                                     ))}
                                                 </h5>
                                             )}
-                                            <h4 className="flex">
-                                                Years of experience :{" "}
-                                                <p className="ml-1">
+                                            <h4 className="flex" dir={t("directionc")}>
+                                                {t("yearsofex")}:{" "}
+                                                <p className="ml-1" dir={t("directionc")}>
                                                     {doc.Experience}
                                                 </p>
                                             </h4>
                                         </div>
-                                        <button className="border-white border-2 px-4 py-2 rounded-full mt-4 transition-all duration-500 hover:bg-white hover:text-black">
+                                        <button dir={t("directionc")} className="border-white border-2 px-4 py-2 rounded-full mt-4 transition-all duration-500 hover:bg-white hover:text-black">
                                             <Link to="/appointment">
-                                                Book An Appointment
+                                            {t("book_appoi")}
                                             </Link>
                                         </button>
                                     </div>
@@ -85,7 +121,7 @@ const Profile = () => {
 
                     <div className="treatmentDetails">
                         <div className="depMain">
-                            <h1 className="depMainHead">Procedures</h1>
+                            <h1 className="depMainHead" dir={t("directionc")}>{t("Procedures")}</h1>
                             <div className="depLine"></div>
                         </div>
 
@@ -99,7 +135,7 @@ const Profile = () => {
                                         />
                                     </div>
                                     <div className="treatmentText">
-                                        <h2 className="drskillTitle">
+                                        <h2 className="drskillTitle" dir={t("directionc")}>
                                             {skill}
                                         </h2>
                                     </div>
@@ -108,9 +144,10 @@ const Profile = () => {
                         </div>
                     </div>
                 </div>
-            ))}
+            ))}</div>)}
+           
         </>
     );
 };
 
-export default Profile;
+export default withTranslation()(Profile);
