c18c0596414beebba116ce93cd8b62f16b6e53fd
ajfuturistics <ajfuturistics@gmail.com>
2022-01-13 12:50:39 +0530

    gg

---
+++ src/components/DemoData/del.js
@@ -0,0 +1,24 @@
+export const del = [
+    {
+        imgUrl: "/assets/dr/maledoc.png",
+        name: "Dr. Muhammed Farouq",
+        Nationality: "Pakistan",
+        Qualification: "MBBS MSC Gastroenterology UK",
+        Experience: " 15 years",
+        position: "Internal Medicine and Gastroenterology Specialist",
+        department: ["gastroenterology"],
+        search: "Gastroenterology",
+        License: ["Member of Saudi Gastroenterology Association"],
+        skills: [
+            "Herpetology and Endoscopy",
+            "Diagnose and treating internal Medicine",
+            "Liver Diseases",
+            "Gastrointestinal",
+            "Pancreatic Diseases",
+        ],
+        mobno: "+00 0000 0000 00",
+        email: "roberto@medwise.com",
+        linkto: "Muhammed_Farouq",
+    },
+];
+export default { del };
