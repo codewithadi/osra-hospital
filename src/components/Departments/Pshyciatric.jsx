fadadbd5314ffec04b403b4a2608710cd73f9d13
Chirag Khatri <kchiragkhatri2001@gmail.com>
2022-01-13 19:46:03 +0530

    departments added

--- src/components/Departments/Pshyciatric.jsx
+++ src/components/Departments/Pshyciatric.jsx
@@ -1,63 +1,101 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./departments.css";
 import { demoData } from "../DemoData/demodata";
+import { demoDatas } from "../DemoData/demodatas";
 import SingleDoctor from "../Doctor/SingleDoctor";
 import { withTranslation } from "react-i18next";
+import Loading from "../Loading/Loading";
 
 function Pshyciatric({ t }) {
-    const treatmentsOffered = {
-        TherapeuticModalities: [
-            {
-                name: `${t("psyc.tm1")}`,
-                detail: "",
-                logo: "",
-            },
-            {
-                name: `${t("psyc.tm2")}`,
-                detail: "",
-                logo: "",
-            },
-        ],
+  const [data, setData] = useState();
+  const [loading, setloading] = useState(true);
+
+  // const filterdDoc = data.filter((data1) =>
+  //     data1.department.includes("bariatic")
+  // );
+  useEffect(() => {
+    setloading(true);
+    const getDept = () => {
+      const language = localStorage.getItem("language");
+      console.log(language === "ar");
+      if (language === "ar") {
+        console.log("ar");
+        const filterdDoc = demoDatas.filter((data1) =>
+          data1.department.includes("pshyciatric")
+        );
+        setData(filterdDoc);
+      } else {
+        console.log("en");
+        const filterdDoc = demoData.filter((data1) =>
+          data1.department.includes("pshyciatric")
+        );
+        setData(filterdDoc);
+      }
+
+      //setDepartment(demodept);
     };
-    const filterdDoc = demoData.filter((data) =>
-        data.department.includes("pshyciatric")
-    );
+    getDept();
+    setloading(false);
+  }, []);
+  const treatmentsOffered = {
+    TherapeuticModalities: [
+      {
+        name: `${t("psyc.tm1")}`,
+        detail: "",
+        logo: "",
+      },
+      {
+        name: `${t("psyc.tm2")}`,
+        detail: "",
+        logo: "",
+      },
+    ],
+  };
+  const filterdDoc = demoData.filter((data) =>
+    data.department.includes("pshyciatric")
+  );
 
-    return (
+  return (
+    <>
+      {loading ? (
+        <div className="w-full h-full flex justify-center items-center py-4">
+          <Loading />
+        </div>
+      ) : (
         <div className="departments">
-            <div className="depMainBack">
-                <img
-                    className="depMainImg"
-                    src="/assets/department/2-min.png"
-                    alt="department Back"
-                />
-                <div className="depMainOverlay">
-                    <h1 className="depMainText">{t("psyc.head")}</h1>
-                </div>
+          <div className="depMainBack">
+            <img
+              className="depMainImg"
+              src="/assets/department/2-min.png"
+              alt="department Back"
+            />
+            <div className="depMainOverlay">
+              <h1 className="depMainText">{t("psyc.head")}</h1>
             </div>
+          </div>
 
-            <div className="depDetail">
-                <div className="depDetailContainer">
-                    <div className="depTextHead">
-                        <h1 className="depDetailTitle">{t("psyc.head")}</h1>
-                        <div className="depDetailAbout">
-                            {t("psyc.para1")}
-                            {/* <br />
+          <div className="depDetail">
+            <div className="depDetailContainer">
+              <div className="depTextHead">
+                <h1 className="depDetailTitle">{t("psyc.head")}</h1>
+                <div className="depDetailAbout">
+                  {t("psyc.para1")}
+                  {/* <br />
                             <br />
                             {t("psyc.para2")} */}
-                        </div>
-                    </div>
-                    <div className="depImgWrapper">
-                        <img
-                            className="depDetailImg"
-                            src="/assets/images/pshyciatric.jpg"
-                            alt="Pshyciatric"
-                        />
-                    </div>
                 </div>
+              </div>
+              <div className="depImgWrapper">
+                <img
+                  className="depDetailImg"
+                  src="/assets/images/pshyciatric.jpg"
+                  alt="Pshyciatric"
+                />
+              </div>
             </div>
+          </div>
 
-            {/* <div className="treatmentDetails">
+          {/* <div className="treatmentDetails">
                 <div className="depMain">
                     <h1 className="depMainHead">{t("to")}</h1>
                     <p className="depMainPara">{t("psyc.topara")}</p>
@@ -89,31 +127,31 @@ function Pshyciatric({ t }) {
                     )}
                 </div>
             </div> */}
-            <div className="depDoctors">
-                <div className="depMain">
-                    <h1 className="depMainHead">{t("depdoc")}</h1>
-                    {/* <p className="depMainPara">
+          <div className="depDoctors">
+            <div className="depMain">
+              <h1 className="depMainHead">{t("depdoc")}</h1>
+              {/* <p className="depMainPara">
                         Furthermore, our Psychiatrists at Osra Hospital you may
                         know:
                     </p> */}
-                    <div className="depLine"></div>
-                </div>
-                <div className="grid grid-cols-1 justify-center justify-items-center items-center gap-4 md:grid-cols-4 px-2 md:px-10">
-                    {filterdDoc.map((doc, index) => (
-                        <SingleDoctor
-                            key={index}
-                            imgUrl={doc.imgUrl}
-                            name={doc.name}
-                            position={doc.position}
-                            mobno={doc.mobno}
-                            email={doc.email}
-                            linkto={doc.linkto}
-                        />
-                    ))}
-                </div>
+              <div className="depLine"></div>
             </div>
+            <div className="grid grid-cols-1 justify-center justify-items-center items-center gap-4 md:grid-cols-4 px-2 md:px-10">
+              {data.map((doc, index) => (
+                <SingleDoctor
+                  key={index}
+                  imgUrl={doc.imgUrl}
+                  namear={doc.namear}
+                  positionar={doc.positionar}
+                  mobno={doc.mobno}
+                  email={doc.email}
+                  linkto={doc.linkto}
+                />
+              ))}
+            </div>
+          </div>
 
-            {/* <div className="depBanner">
+          {/* <div className="depBanner">
                 <img
                     class="depBannerImg"
                     src="/assets/department/depLastBack.jpg"
@@ -137,7 +175,9 @@ function Pshyciatric({ t }) {
                 </div>
             </div> */}
         </div>
-    );
+      )}
+    </>
+  );
 }
 
 export default withTranslation()(Pshyciatric);
