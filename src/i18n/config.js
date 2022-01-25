82d5026be91987551aace21e4748e7ba8946d2c8
ajfuturistics <ajfuturistics@gmail.com>
2022-01-25 18:27:03 +0530

    gg

--- src/i18n/config.js
+++ src/i18n/config.js
@@ -3,7 +3,7 @@ import { initReactI18next } from "react-i18next";
 
 i18n.use(initReactI18next).init({
     fallbackLng: "ar",
-    lng: "ar",
+    lng: "ar", // default Lang
     resources: {
         en: {
             translations: require("./locales/en/translations.json"),
