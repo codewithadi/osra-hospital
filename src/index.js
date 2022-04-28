e2d9e2749932c5a5c197b81dffe1071e47973886
AJFuturistic <ajfuturistics@gmail.com>
2022-04-28 10:26:22 +0530

    added maintenance page

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import "./i18n/config";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import Maintenance from "./pages/Maintenance";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>{<App />}</BrowserRouter>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
